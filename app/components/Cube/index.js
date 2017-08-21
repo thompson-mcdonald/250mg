d5eac9e40bcd61ead8f69459a8ee0743d34004f8
jimmynames <jim.me.ryan@gmail.com>
2017-08-21 21:36:45 +0100

    text effect on title

---
+++ app/components/Cube/index.js
@@ -0,0 +1,60 @@
+import React from 'React';
+import styled from 'styled-components';
+import H1 from 'components/H1';
+
+const Enviro = styled.div`
+	-webkit-perspective: 1000px;
+	position: fixed;
+	z-index: 99999999999;
+	text-align: center;
+	margin: 0 auto;
+	width: 100%;
+`
+
+const CubeDice = styled.div`
+	width: 50%;
+	text-align: center;
+	margin: 0 auto;
+	height: 100px;
+	transition: transform 1.33s; /* Animate the transform properties */
+	transform-style: preserve-3d; /* <-NB */
+	&:hover {
+		transform: rotateX(90deg); /* Text bleed at 90º */
+	}
+`;
+
+const CubeSide1 = styled.div`
+background: transparent;
+height: 100px;
+transform: translateZ(100px);
+h1 {
+	color: black;
+}
+`
+
+const CubeSide2 = styled.div`
+background: transparent;
+height: 100px;
+transform: rotateX(-90deg) translateZ(-100px);
+
+h1 {
+	color: white;
+}
+`
+
+export default class Cube extends React.PureComponent {
+	render() {
+		return (
+		<Enviro>
+			<CubeDice>
+				<CubeSide1>
+					<H1>250mg</H1>
+				</CubeSide1>
+				<CubeSide2>
+					<H1>names36</H1>
+				</CubeSide2>
+			</CubeDice>
+	</Enviro>
+		)
+	}
+};
--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -13,6 +13,7 @@ import React from 'react';
 import messages from './messages';
 import styled from 'styled-components';
 import H1 from 'components/H1';
+import Cube from 'components/Cube';
 
 let visList = [
   "https://www.youtube.com/embed/Tpba70L2BLo?ecver=2&loop=1&autoplay=1&showinfo=0&mute=1&controls=0&vq=244",
@@ -74,6 +75,8 @@ const Video = styled.div`
 const Container = styled.div`
   height: 100%;
   width:100%
+  display: flex;
+  justify-content: center;
 `
 
 const Slide1 = styled.iframe`
@@ -98,12 +101,13 @@ export default class HomePage extends React.PureComponent { // eslint-disable-li
     return (
       <Video>
         <Container className="music">
-          <H1>250mg</H1>
+
+          <Cube />
+          
           <Slide3 src={printLine(vis, 1)} width="100%" height="100%" frameBorder="0" autoplay='true' allowFullScreen></Slide3>
           <Slide2 src={printLine(vis, 2)} width="100%" height="100%" frameBorder="0" autoplay='true' allowFullScreen></Slide2>
           <Slide1 src={printLine(music, 2)} width="100%" height="100%" frameBorder="0" autoplay='true' allowFullScreen></Slide1>
         </Container>
-
       </Video>
     );
   }
